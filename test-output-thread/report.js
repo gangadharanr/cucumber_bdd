6d275c8c86504b06a69095da55ef4346e8737d74
unknown <gangadharanr7@gmail.comgit config --global user.name gangadharanr>
2023-09-11 00:29:13 +0530

    added librabry

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "e6c82eef-d65f-4480-a17c-55ecdaf400e5",
+    "feature": "Demo Project for company",
+    "scenario": "Accounts page - title",
+    "start": 1694372276497,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1694372299254,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[#1,main,5,main]"
+  }
+]);
+});

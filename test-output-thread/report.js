4c3245cb39f0362756124e7ce0e37e4ad6a0be10
unknown <gangadharanr7@gmail.comgit config --global user.name gangadharanr>
2023-09-10 23:51:48 +0530

    updated chrome

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "f99548a8-f715-4a8a-b7db-e0def37c353d",
+    "id": "6b7dd1ed-1330-4927-8747-c16963dfa77d",
     "feature": "Demo Project for company",
     "scenario": "Accounts page - title",
-    "start": 1694367524665,
+    "start": 1694370061489,
     "group": 1,
     "content": "",
     "tags": "@accounts,",
-    "end": 1694367551881,
-    "className": "passed"
+    "end": 1694370065714,
+    "className": "failed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
